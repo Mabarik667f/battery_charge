07c1eb76db0ae61dcfab589d009543ba48c66e47
Mabarik667f <jc.bob1412@gmail.com>
2024-10-12 00:49:21 +0300

    feature: create script, logic (no refactoring, no tests, no demonization)

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,49 @@
+use std::env;
 use std::fs;
 use std::path;
 
-pub struct Battery {
-    capacity: u8,
-    status: String,
+pub struct Config {
+    pub time_period: u8,
+    pub min_lifetime_percentage: u8,
 }
 
-pub struct Config {
-    time_period: u8,
-    min_lifetime_percentage: u8,
-    notification: String,
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let time_period = match args.next() {
+            Some(arg) => match arg.parse::<u8>() {
+                Ok(arg) => arg,
+                _ => return Err("time_period Expected integer!"),
+            },
+            None => return Err("Didn't get a time_period minute time"),
+        };
+
+        let min_lifetime_percentage = match args.next() {
+            Some(arg) => match arg.parse::<u8>() {
+                Ok(arg) => arg,
+                _ => return Err("min_lifetime_percentage Expected integer!"),
+            },
+            None => return Err("Didn't get a min_lifetime_percentage"),
+        };
+
+        Ok(Config {
+            time_period,
+            min_lifetime_percentage,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct Battery {
+    capacity: u8,
+    status: ChargingStatus,
+}
+
+#[derive(Debug)]
+pub enum ChargingStatus {
+    Charging,
+    Notcharging,
 }
 
 impl Battery {
@@ -20,6 +54,8 @@ impl Battery {
     }
 }
 
+pub fn run() {}
+
 fn get_battery_capacity(path: &str) -> u8 {
     let abs_path = path::absolute(format!("{path}/capacity")).unwrap();
     fs::read_to_string(abs_path)
@@ -29,15 +65,26 @@ fn get_battery_capacity(path: &str) -> u8 {
         .unwrap()
 }
 
-fn get_battery_status(path: &str) -> String {
+fn get_battery_status(path: &str) -> ChargingStatus {
     let abs_path = path::absolute(format!("{path}/status")).unwrap();
-    fs::read_to_string(abs_path).unwrap()
+    match fs::read_to_string(abs_path)
+        .unwrap()
+        .replace(" ", "")
+        .trim()
+        .to_string()
+        .as_str()
+    {
+        "Notcharging" | "Discharging" => ChargingStatus::Notcharging,
+        "Charging" => ChargingStatus::Charging,
+        _ => panic!("Undefined status!"),
+    }
 }
 
 pub fn get_all_batteries() -> Vec<Battery> {
     let abs_path = path::absolute("/sys/class/power_supply/").unwrap();
     let mut batteries: Vec<Battery> = Vec::new();
     let mut c = 0;
+
     loop {
         let path_to_bat = format!("{}/BAT{}", abs_path.to_str().unwrap(), c);
         let p = &path_to_bat;
@@ -50,13 +97,38 @@ pub fn get_all_batteries() -> Vec<Battery> {
         println!("{bat:#?}");
         c += 1
     }
+    println!("{batteries:#?}");
     batteries
 }
 
-fn get_total_capacity(batteries: Vec<Battery>) -> u8 {
+pub fn get_data_for_batteries(batteries: Vec<Battery>) -> bool {
+    let batteries_ref = &batteries;
+    let total_cap = get_total_capacity(batteries_ref);
+    let is_device_charging = any_battery_charging(batteries_ref);
+
+    if total_cap > 20 || is_device_charging {
+        false
+    } else {
+        true
+    }
+}
+
+fn get_total_capacity(batteries: &Vec<Battery>) -> u8 {
     let mut total_cap = 0;
+    let mut c = 0;
     for bat in batteries {
         total_cap += bat.capacity;
+        c += 1;
     }
-    total_cap
+    total_cap / c
+}
+
+fn any_battery_charging(batteries: &Vec<Battery>) -> bool {
+    for bat in batteries {
+        match bat.status {
+            ChargingStatus::Charging => return true,
+            _ => continue,
+        }
+    }
+    false
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use battery_charge::{get_all_batteries, get_data_for_batteries, Config};
+use chrono::Utc;
+use cron::Schedule;
+use std::env;
+use std::process;
+use std::process::Command;
+use std::str::FromStr;
+use std::thread;
+use toml_parser::read_notification_data;
+
+pub mod toml_parser;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    let batteries = get_all_batteries();
+    let a = get_data_for_batteries(batteries);
+    println!("{a}");
+
+    let notification_data = read_notification_data().unwrap();
+    let args = vec![
+        notification_data.config.title,
+        notification_data.config.text,
+    ];
+    let exp = format!("0 */{} * * * *", config.time_period);
+    let schedule = Schedule::from_str(&exp).unwrap();
+    loop {
+        let datetime = schedule.upcoming(Utc).next().unwrap();
+        let until = datetime - Utc::now();
+        thread::sleep(until.to_std().unwrap());
+        let e = Command::new("notify-send").args(&args).spawn();
+        println!("ERRORS: {e:#?}");
+    }
 }
